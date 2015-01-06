efc080241340ce4313c19ace686cd4edcc965abc
Lioznov Anton <Lavton@gmail.com>
2015-01-06 19:12:53 +0300

    works with mySQL

--- login.php
+++ login.php
@@ -15,27 +15,14 @@
     include('menu.html')
   ?>
 
-<form action="response.php" class="js-ajax-php-json" method="post" accept-charset="utf-8">
-  <input type="text" name="favorite_beverage" value="" placeholder="Favorite restaurant" />
-  <input type="text" name="favorite_restaurant" value="" placeholder="Favorite beverage" />
-  <select name="gender">
-    <option value="male">Male</option>
-    <option value="female">Female</option>
-  </select>
-  <input type="submit" name="submit" value="Submit form"  />
-</form>
-
-<div class="the-return">
-  [HTML is replaced when successful.]
-</div>
   <div class="container">
     <form class="form-signin" role="form">
       <h2 class="form-signin-heading">Please sign in</h2>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputUser" class="form-control" placeholder="Username" required="" autofocus="">
-      <label for="inputPassword" class="sr-only">Password</label>
+<!--       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+ -->      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
   </div>
 
@@ -45,52 +32,23 @@
   <script type="text/javascript" src="standart/js/jstree.js"></script>
   <script type="text/javascript" src="standart/js/bootstrap.js"></script>
 
-<script type="text/javascript">
-// $("document").ready(function(){
-  $(".js-ajax-php-json").submit(function(){
-    var data = {
-      "action": "test"
-    };
-    data = $(this).serialize() + "&" + $.param(data);
-    console.log($(this).serialize())
+  <script>
+  $(".btn").click(function() {
+    console.log("Hello");
+    data =  {username: $("#inputUser").val(),};
     $.ajax({
       type: "POST",
+      url: "/r_login.php",
       dataType: "json",
-      url: "response.php",
-      data: data,
-      success: function(data) {
-        $(".the-return").html(
-          "Favorite beverage: " + data["favorite_beverage"] + "<br />Favorite restaurant: " + data["favorite_restaurant"] + "<br />Gender: " + data["gender"] + "<br />JSON: " + data["json"]
-        );
-
-        alert("Form submitted successfully.\nReturned json: " + data["json"]);
-      }
-    });
-    return false;
+      data:  $.param(data)
+    }).done(function(json) {
+      console.log(json);
+      console.log('success');
+    }).fail(function() {
+      alert("No user with such data.");
+    });;
   });
-// });
-</script>
-
-
-<script>
-$(".btn").click(function() {
-  console.log("Hello");
-  data =  {username: $("#inputUser").val(),};
-  $.ajax({
-    type: "POST",
-    // dataType: "json",
-    url: "/r_login.php",
-    dataType: "json",
-    data:  $.param(data), success: function(data) {
-        $(".the-return").html(
-          "JSON: " + data["json"]
-        );
-
-        alert("Form submitted successfully.\nReturned json: " + data["json"]);
-      }
-  });
-});
-</script>
+  </script>
 
 </body>
 </html>
--- r_login.php
+++ r_login.php
@@ -8,26 +8,31 @@ function is_ajax() {
   return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
 }
 
+//return data of authorization
 function auth_function(){
-  // $return = $_POST;
-  
-  //Do what you need to do with the info. The following are some examples.
-  //if ($return["favorite_beverage"] == ""){
-  //  $return["favorite_beverage"] = "Coke";
-  //}
-  //$return["favorite_restaurant"] = "McDonald's";
-  
-  // $return["json"] = json_encode($return);
-$return = $_POST;
+	$return = $_POST;
 	
-	//Do what you need to do with the info. The following are some examples.
-	//if ($return["favorite_beverage"] == ""){
-	//	$return["favorite_beverage"] = "Coke";
-	//}
-	//$return["favorite_restaurant"] = "McDonald's";
-	
-	$return["json"] = json_encode($return);
-	echo json_encode($return);
-  // echo json_encode($return);
+	$link = mysql_connect('127.0.0.1', 'lavton', 'qwerty')
+    or die('Не удалось соединиться: ' . mysql_error());
+	mysql_select_db('constellation') or die('Не удалось выбрать базу данных');
+	// Выполняем SQL-запрос
+	@mysql_query("Set charset utf8");
+	@mysql_query("Set character_set_client = utf8");
+	@mysql_query("Set character_set_connection = utf8");
+	@mysql_query("Set character_set_results = utf8");
+	@mysql_query("Set collation_connection = utf8_general_ci");
+
+	$query = 'SELECT * FROM users where name=\''.$_POST["username"].'\';';
+	$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
+	$result = mysql_fetch_array($result, MYSQL_ASSOC);
+	if (isset($result["user_id"])) {
+		$result["result"] = "Success";
+	} else {
+		$result["result"] = "Fail";
+	}
+/*	
+	$return["json"] = json_encode($result);
+*/	
+	echo json_encode($result);
 }
 ?>
