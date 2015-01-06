09c071d0c5d669dfee964a7c5ed32bebe2737921
Lioznov Anton <Lavton@gmail.com>
2015-01-06 22:12:55 +0300

    magic with htaccess

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="/own/images/icon.ico">
     <title>CПО "СОзвездие" | будущий сайт отряда</title>
-    <link rel="stylesheet" href="standart/css/bootstrap.css"/>
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/standart/css/bootstrap.css"/>
+    <link rel="stylesheet" href="/standart/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="own/css/common_style.css">
 </head>
 <body>
