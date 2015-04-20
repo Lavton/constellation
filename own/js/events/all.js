394161b2dc3ca129ad089d5069b147d8971875f4
Lioznov Anton <Lavton@gmail.com>
2015-04-20 17:18:43 +0300

    add UI and move temrary to admin zone

--- own/js/events/all.js
+++ own/js/events/all.js
@@ -1,20 +1,92 @@
 $('#page-container').on('click', ".pre-add-new-event", function() {
-  var today = new Date();
-  var start_date = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
-  var finish_date = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
-  var data = {
-    action: "add_new_event", 
-    start_date: start_date,
-    finish_date: finish_date
+  var auto_date = null;
+  if (! $(".pre-add-new-event").hasClass("clicked")) {
+    $(".add-new-input-w").removeClass("hidden")
+    $(".pre-add-new-event").addClass("clicked")
+    $(".pre-add-new-event").text("Добавить")
+    auto_date = setInterval(function(){
+      var start_date = $(".add-new-event-start-date").val();
+      var end_date = $(".add-new-event-end-date").val();
+      if (end_date == "") {
+        if (start_date != "") {
+          $(".add-new-event-end-date").val(start_date);
+        }
+      } else {
+        clearInterval(auto_date);
+      }
+    }, 750);
+
+  } else {
+    var name = $(".add-new-event-name").val();
+    var start_date = $(".add-new-event-start-date").val();
+    var start_time = $(".add-new-event-start-time").val();
+    var end_date = $(".add-new-event-end-date").val();
+    var end_time = $(".add-new-event-end-time").val();
+    debugger;
+
+
   }
-  $.ajax({ //TODO: make with angular
-    type: "POST",
-    url: "/handlers/event.php",
-    dataType: "json",
-    data:  $.param(data)
-  }).done(function(response) {
-    if (response.result == "Success") {
-      window.location="/events/event/"+response.id;
-    }
-  });
-});
+/*    var data = {user_ids: $(".add-new-fighter-d").val(), fields: ["contacts", "bdate"]}
+    $.ajax({ //TODO: make with angular
+      type: "GET",
+      url: "https://api.vk.com/method/users.get",
+      dataType: "jsonp",
+      data:  $.param(data)
+    }).done(function(vk_response) {
+      if (vk_response.response) {
+        $(".add-new-fighter-d").addClass("own-valid")
+        $(".add-new-fighter-d").removeClass("own-invalid")
+        if (_.findWhere(id_and_uid, {vk_id: vk_response.response[0].uid+""})) {
+          $(".add-new-fighter-d").addClass("own-invalid")
+          $(".add-new-fighter-d").removeClass("own-valid")
+        } else {
+          $(".add-new-fighter-d").addClass("own-valid")
+          $(".add-new-fighter-d").removeClass("own-invalid")
+        }
+
+      } else {
+        $(".add-new-fighter-d").addClass("own-invalid")
+        $(".add-new-fighter-d").removeClass("own-valid")
+      }
+
+      //добавляем нового пользователя лишь если данные корректны
+      if ($(".add-new-fighter-d").hasClass("own-valid") && $(".add-new-fighter-id").hasClass("own-valid")) {
+        var vk_user = vk_response.response[0];
+        var bd = [];
+        if (vk_user.bdate){
+          vk_user.bdate.split(".");
+        }
+        if (!bd[0]) {
+          bd[0] = "01"
+        }
+        if (!bd[1]) {
+          bd[1] = "01"
+        }
+        if (!bd[2]) {
+          bd[2] = "0001"
+        }
+        bd = bd[2]+"-"+bd[1]+"-"+bd[0]; 
+        var send_data = {
+          action: "add_new_fighter", 
+          id: $(".add-new-fighter-id").val()*1, 
+          vk_id: vk_user.uid+"",
+          name: vk_user.first_name,
+          surname: vk_user.last_name,
+          birthdate: bd,
+          year_of_entrance: (new Date()).getFullYear(),
+          group_of_rights: 3
+        }
+        $.ajax({ //TODO: make with angular
+          type: "POST",
+          url: "/handlers/user.php",
+          dataType: "json",
+          data:  $.param(send_data)
+        }).done(function(response) {
+          if (response.result == "Success") {
+            window.location="/about/users/"+$(".add-new-fighter-id").val();
+          }
+        });
+      }
+    });
+  }
+*/});
--- own/templates/events/all.php
+++ own/templates/events/all.php
@@ -2,11 +2,12 @@
 
 
 <?php /*комсостав+ может добавлять новые мероприятия*/
-if ((isset($_SESSION["current_group"]) && ($_SESSION["current_group"] >= COMMAND_STAFF))) {
+if ((isset($_SESSION["current_group"]) && ($_SESSION["current_group"] >= ADMIN))) {
 ?><br/><br/><br/>
   <button type="button" class="btn btn-warning pre-add-new-event" >Создать мероприятие?</button> 
- <span class="add-new-input-w hidden">  vk.com/<input type="text" class="add-new-fighter-d" placeholder="введите домен вконтакте" size=30 />
-         id: <input type="number" class="add-new-fighter-id" />
-
-  </span>
+ <div class="add-new-input-w hidden">	
+ название: <input type="text" class="add-new-event-name" placeholder="название мероприятия" size=50 /> <br>
+ дата и время начала: <input type="date" class="add-new-event-start-date" /> <input type="time" class="add-new-event-start-time"/><br>
+ дата и время конца: <input type="date" class="add-new-event-end-date" /> <input type="time" class="add-new-event-end-time"/><br>
+  </div>
 <?php } ?>  
