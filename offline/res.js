043c3bccd3750914aa1c1aceaf4af01546e6111e
Anton Lioznov <Lavton@gmail.com>
2015-11-03 11:12:13 +0300

    people offline almost done

--- offline/res.js
+++ offline/res.js
@@ -5,13 +5,8 @@
     }
     return tel ? "+7 (" + tel[0] + tel[1] + tel[2] + ") " + tel[3] + tel[4] + tel[5] + "-" + tel[6] + tel[7] + "-" + tel[8] + tel[9] : ""
   }
-  console.log("hello");
   window.people = JSON.parse(window.localStorage.getItem("people"))
   var result = _.template($("#people_temp").html())
-  console.log(result({
-    "users": window.people
-  }))
-  console.log(window.people)
   $("#container").append(result({
     "users": window.people
   }));
